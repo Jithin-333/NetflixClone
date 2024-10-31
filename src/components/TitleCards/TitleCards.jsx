b5e4e8bc4aefd093a0d182dcc834b81f0e4b9558
jithin-333 <jithin-333>
2024-10-31 13:35:56 +0530

    first commit

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useRef, useState } from 'react'
+import './TitleCards.css'
+import { Link } from 'react-router-dom';
+
+// import cards_data from '../../assets/cards/Cards_data'
+
+
+
+const TitleCards = ({title, category}) => {
+
+    const [apiData, setApiData] = useState([]);
+
+  const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NGJiY2NiNTFkNjVjNmY2ZWFjYTMwOTc3OWRhZWQyOCIsIm5iZiI6MTcyODkwMTU2OC41MDU3NTQsInN1YiI6IjY3MGNlZmI4M2JiNDU1N2M2NjljMGNlNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2ftaqeNyVNSJzVR2GSLUHdrlfQz5HcQpL29IN7v42q4'
+    }
+  };
+  
+  
+
+  const handleWheel = (event) =>{
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+
+  }
+
+  useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${category ? category:"now_playing"}?language=en-US&page=1`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results))
+    .catch(err => console.error(err));
+
+    cardsRef.current.addEventListener('wheel',handleWheel);
+    
+  },[])
+
+  
+  return (
+    <div className='title-cards'>
+
+      <h2>{title? title:"Popular on Netflix"}</h2>
+
+      <div className="card-list" ref={cardsRef}>
+        {apiData.map((card,index) => {
+          return <Link to={`/player/${card.id}`} className="card" key = {index}>
+            <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+            <p>{card.original_title}</p>
+          </Link>
+        })}
+      </div>
+      
+    </div>
+  )
+}
+
+export default TitleCards
